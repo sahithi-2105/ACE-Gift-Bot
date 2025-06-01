90d9d57023233346c552bc995620b87b1cb9217d
sahithi-2105 <sahithisiddireddy05@gmail.com>
2025-06-01 22:17:52 +0530

    Update constants.js

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,2 +1,2 @@
 // export const baseUrl = "http://localhost:3000/api";
-export const baseUrl = "https://ACE-GiftBot-backend.onrender.com/api";
+export const baseUrl = "https://ace-giftbot-backend.onrender.com/api";
